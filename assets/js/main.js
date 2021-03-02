3b9d7d9292a4c2335dc8aa5e47a838b1fb9e626e
wass01 <maatallawassim01@gmail.com>
2021-03-02 15:09:22 +0100

    creazione file

---
+++ assets/js/main.js
@@ -0,0 +1,3 @@
+// creazione lista delle persone che possono accedere
+var listaEmail = ["pippo@gmail.com", "maatallawassim01@gmail.com", "pluto30@hotmail.it", "topolino@yahoo.com"];
+var email = prompt("Inserisci la tua email:");
