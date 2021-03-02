09cf3c971f21d723de634b9b4de2794f06b71e3a
wass01 <maatallawassim01@gmail.com>
2021-03-02 15:15:35 +0100

    Esercizio email terminato

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,3 +1,18 @@
-// creazione lista delle persone che possono accedere
+// esercizio email
+// creazione variabili necessarie
 var listaEmail = ["pippo@gmail.com", "maatallawassim01@gmail.com", "pluto30@hotmail.it", "topolino@yahoo.com"];
 var email = prompt("Inserisci la tua email:");
+var condizione = false;
+
+// creazione ciclo
+for (var i = 0; i < listaEmail.length; i++) {
+  if (email == listaEmail[i]){
+    condizione = true;
+  }
+}
+
+if (condizione){
+  console.log("La tua email è nella lista");
+} else {
+  console.log("Mi dispiace la tua email non è nella lista");
+}
